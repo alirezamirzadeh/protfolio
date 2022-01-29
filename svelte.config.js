83f6827bf3de8e4f92f69910627e175b4af7f27e
alirezamirzadeh <rexalirz72@gmail.com>
2022-01-29 11:00:56 +0330

    change svelte.config

--- svelte.config.js
+++ svelte.config.js
@@ -6,8 +6,7 @@ import adapter from '@sveltejs/adapter-vercel';
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
 
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
+	
 	preprocess: [mdsvex(mdsvexConfig)],
 
 	kit: {
@@ -15,11 +14,6 @@ const config = {
 		adapter: adapter(),
 
 
-		// if you are not using the static adapter and
-		// you don't want prerendering, remove this section
-		prerender: {
-			entries: ['*', '/sitemap.xml', '/rss.xml']
-		},
 
 		vite: {
 			// allows vite access to ./posts
@@ -33,23 +27,3 @@ const config = {
 };
 
 export default config;
-
-// import { mdsvex } from 'mdsvex';
-// import mdsvexConfig from './mdsvex.config.js';
-// import adapter from '@sveltejs/adapter-auto';
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	extensions: ['.svelte', ...mdsvexConfig.extensions],
-
-// 	kit: {
-// 		adapter: adapter(),
-
-// 		// hydrate the <div id="svelte"> element in src/app.html
-// 		target: '#svelte'
-// 	},
-
-// 	preprocess: [mdsvex(mdsvexConfig)]
-// };
-
-// export default config;
