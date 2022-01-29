2bdf17acb349b867aa1114bc1be90713a6c97163
alirezamirzadeh <rexalirz72@gmail.com>
2022-01-29 17:32:01 +0330

    edit

--- src/lib/PageTransition.svelte
+++ src/lib/PageTransition.svelte
@@ -7,7 +7,7 @@
   
   {#key refresh}
     <div
-      in:fly={{ x: -5, duration: 500, delay: 500 }}
+      in:fly={{ x: -5, duration: 500}}
       out:fly={{ x: 5, duration: 500 }}
     >
       <slot />
