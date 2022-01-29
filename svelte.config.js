c1af08048cebe0ce5f457cd923dcda0c6d078dbd
alirezamirzadeh <rexalirz72@gmail.com>
2022-01-29 10:55:50 +0330

    change pavkage json

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
