22ec82cff8161809cd8fea939f54e134f4ef3e01
alirezamirzadeh <rexalirz72@gmail.com>
2022-01-29 10:27:47 +0330

    camecase env

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,8 @@
 import {createClient} from '@supabase/supabase-js';
 
 
-const supabaseUrl =import.meta.env.VITE_PUBLIC_SUBSCRIBE_URL;
-const supabaseAnonKey =import.meta.env.VITE_PUBLIC_SUBSCRIBE_ANON_Key;
+const supabaseUrl =import.meta.env.VITEPUBLICSUBSCRIBEURL;
+const supabaseAnonKey =import.meta.env.VITEPUBLICSUBSCRIBEANONKEY;
 
 
 export const supabase = createClient(supabaseUrl,supabaseAnonKey)
