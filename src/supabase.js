a1d7564b97dd96d698350b6be609fc9b6788f143
alirezamirzadeh <rexalirz72@gmail.com>
2022-02-02 11:54:49 +0330

    version 1

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,8 @@
 import {createClient} from '@supabase/supabase-js';
 
 
-const supabaseUrl =process.env.VITEPUBLICSUBSCRIBEURL;
-const supabaseAnonKey =process.env.VITEPUBLICSUBSCRIBEANONKEY;
+const supabaseUrl =process.env.VITE_PUBLIC_SUBSCRIBE_URL
+;
+const supabaseAnonKey =process.env.VITE_PUBLIC_SUBSCRIBE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl,supabaseAnonKey)
